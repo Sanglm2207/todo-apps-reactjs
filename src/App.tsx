64db5cab6efb424420d944753c9db45ab3de2802
SangLM <sanglm@funix.edu.vn>
2022-11-17 09:53:34 +0700

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import ProgressContextProvider from './contexts/ProgressContext'
+import ToggleTheme from './components/ToggleTheme'
+import ThemeContextProvider from './contexts/ThemeContext'
+import Movies from './components/Movies'
+import MovieContextProvider from './contexts/MovieContext'
+import TopMovieContextProvider from './contexts/TopMovieContext'
+import TopMovies from './components/TopMovies'
+import { Grid } from '@material-ui/core'
+import AuthContextProvider from './contexts/AuthContext'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<AuthContextProvider>
+				<TopMovieContextProvider>
+					<MovieContextProvider>
+						<ThemeContextProvider>
+							<ProgressContextProvider>
+								<Navbar />
+								<Grid container>
+									<Grid item xs={4}>
+										<TopMovies />
+									</Grid>
+									<Grid item xs={8}>
+										<Movies />
+									</Grid>
+								</Grid>
+								<ToggleTheme />
+							</ProgressContextProvider>
+						</ThemeContextProvider>
+					</MovieContextProvider>
+				</TopMovieContextProvider>
+			</AuthContextProvider>
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/ToggleTheme.tsx
@@ -0,0 +1,38 @@
+import { Fab } from "@material-ui/core";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+import FormatColorFillOutlinedIcon from "@material-ui/icons/FormatColorFillOutlined";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    floatBtn: {
+      position: "fixed",
+      right: "3rem",
+      bottom: "3rem",
+    },
+  })
+);
+
+const ToggleTheme = () => {
+  const classes = useStyles();
+
+  // context
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  return (
+    <Fab
+      color="primary"
+      variant="extended"
+      className={classes.floatBtn}
+      onClick={toggleTheme.bind(
+        this,
+        theme === "primary" ? "secondary" : "primary"
+      )}
+    >
+      <FormatColorFillOutlinedIcon />
+    </Fab>
+  );
+};
+
+export default ToggleTheme;
